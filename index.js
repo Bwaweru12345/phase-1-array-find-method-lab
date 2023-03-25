8a6798564f9e1b7a6a8e530a421e6122f5c13808
Bwaweru12345 <bryanwaweru1076@example.com>
2023-03-25 12:25:44 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+  }
+
+  const records = [
+    { year: 2022, result: 'L' },
+    { year: 2021, result: 'W' },
+    { year: 2020, result: 'L' },
+    { year: 2019, result: 'W' },
+    { year: 2018, result: 'L' },
+  ];
+  
+  console.log(superbowlWin(records)); 
